476b065efbcdf892ef313a5dad153b83750c3f57
Joel Kuzmarski <leoj3n@gmail.com>
2015-01-13 23:24:21 -0600

    Update update script

--- nowhere/update_submodules.sh
+++ nowhere/update_submodules.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+DOTFILES="${HOME}/.homesick/repos/dotfiles"
+
 echo
 echo 'Updating Submodules...'
 echo
 
-cd .. && git submodule foreach git pull
+cd "${DOTFILES}/nowhere" && git submodule foreach git pull
 
 echo
-echo 'Building YouTubeCenter...'
+echo 'Build YouTubeCenter...'
 echo
 
-cd submodules/chrome/YouTubeCenter && ant copy-chrome
+cd "${DOTFILES}/submodules/chrome/YouTubeCenter" && ant copy-chrome
 
 echo
 echo 'Unzip Octotree...'
 echo
 
-cd ../octotree/dist && rm -rf chrome && unzip chrome.zip -d chrome
+cd "${DOTFILES}/submodules/chrome/octotree/dist" && rm -rf 'chrome' && unzip 'chrome.zip' -d 'chrome'
+
+echo
+echo 'Build Vimium...'
+echo
+
+cd "${DOTFILES}/submodules/chrome/vimium" && cake build
 
 echo
 echo 'Finished updating submodules.'
