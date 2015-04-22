e9bf587e491ff4604dcac90e0ed2386af1f1a533
Joel Kuzmarski <leoj3n@gmail.com>
2015-04-22 02:46:18 -0500

    Update submodules and script

--- nowhere/update_submodules.sh
+++ nowhere/update_submodules.sh
@@ -6,7 +6,7 @@ echo
 echo 'Updating Submodules...'
 echo
 
-cd "${DOTFILES}/nowhere" && git submodule foreach git pull
+cd "${DOTFILES}" && git submodule init && git submodule update --remote
 
 echo
 echo 'Build YouTubeCenter...'
@@ -21,19 +21,48 @@ echo
 cd "${DOTFILES}/submodules/chrome/octotree/dist" && rm -rf 'chrome' && unzip 'chrome.zip' -d 'chrome'
 
 echo
+echo 'Build uBlock...'
+echo
+
+cd "${DOTFILES}/submodules/chrome/uBlock" && sh "${DOTFILES}/submodules/chrome/uBlock/tools/make-chromium.sh"
+
+echo
 echo 'Build Vimium...'
 echo
 
-cd "${DOTFILES}/submodules/chrome/vimium" && cake build
+cd "${DOTFILES}/submodules/chrome/vimium" && npm install && cake build
 
 echo
 echo 'Build devtools theme (zero dark matrix)...'
 echo
 
-cd "${DOTFILES}/submodules/chrome/zero-base-themes" && grunt
+cd "${DOTFILES}/submodules/chrome/zero-base-themes" && npm install && grunt
 
 echo
 echo 'Finished updating submodules.'
 echo
 
+echo 'GitHub: Blame Parent: ~/.homesick/repos/dotfiles/submodules/chrome/blame_parent'
+echo 'Avatars for Github: ~/.homesick/repos/dotfiles/submodules/chrome/chrome-github-avatars/app'
+echo 'Tab Modifier: ~/.homesick/repos/dotfiles/submodules/chrome/chrome-tab-modifier'
+echo 'Github Annotator: ~/.homesick/repos/dotfiles/submodules/chrome/github-annotator'
+echo 'GitHub Notification Helper for Gmail: ~/.homesick/repos/dotfiles/submodules/chrome/github-gmail/chrome'
+echo 'GitHub Notifier: ~/.homesick/repos/dotfiles/submodules/chrome/github-notifier-chrome/extension'
+echo 'Github Omnibox: ~/.homesick/repos/dotfiles/submodules/chrome/Github-Omnibox'
+echo 'GitHub PR Helper: ~/.homesick/repos/dotfiles/submodules/chrome/github-pr-helper'
+echo 'Grooveshark Enhancement Suite: ~/.homesick/repos/dotfiles/submodules/chrome/grooveshark-enhancement-suite'
+echo 'Isometric Contributions: ~/.homesick/repos/dotfiles/submodules/chrome/isometric-contributions/chrom'
+echo 'jetzt: ~/.homesick/repos/dotfiles/submodules/chrome/jetzt'
+echo 'jQuery Audit: ~/.homesick/repos/dotfiles/submodules/chrome/jquery-audit'
+echo 'MDN Search: ~/.homesick/repos/dotfiles/submodules/chrome/mdn-search'
+echo 'Octotree: ~/.homesick/repos/dotfiles/submodules/chrome/octotree/dist/chrome'
+echo 'Polymer Ready: ~/.homesick/repos/dotfiles/submodules/chrome/polymer-ready-chrome-extension/src'
+echo 'Github Node.js require() navigator: ~/.homesick/repos/dotfiles/submodules/chrome/require-navigator'
+echo 'Scrollbar diet: ~/.homesick/repos/dotfiles/submodules/chrome/scrollbar-diet'
+echo 'Tab Snooze: ~/.homesick/repos/dotfiles/submodules/chrome/tab-snooze'
+echo 'Tinysong for Google Chrome™: ~/.homesick/repos/dotfiles/submodules/chrome/tinysong-chrome-extension'
+echo 'uBlock: ~/.homesick/repos/dotfiles/submodules/chrome/uBlock/dist/build/uBlock.chromium'
+echo 'Vimium: ~/.homesick/repos/dotfiles/submodules/chrome/vimium'
+echo 'YouTube Center Developer Build: ~/.homesick/repos/dotfiles/submodules/chrome/YouTubeCenter/build/YouTubeCenter'
+echo 'DevTools Theme: Zero Dark Matrix: ~/.homesick/repos/dotfiles/submodules/chrome/zero-base-themes/theme-extension'
 
